52518c026daf4820e3f5a72aec09a7dc6149eb37
Corderslite <abrahamgreatebele@gmail.com>
2025-08-27 15:05:58 +0100

    update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://cmom.leathree.com',
-    'https://cmom.leathree.com/',
+    'https://cmom.leathree.com',
     'http://localhost:3001',
     'http://localhost:3000'
   ],
